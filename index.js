a0220e4f941e4cb3f35d546135a4aa4c3853527f
Domm <ademkh611@gmail.com>
2023-07-28 14:43:08 +0200

    checkpoint.finale

---
+++ index.js
@@ -0,0 +1,64 @@
+function program(){
+    document.getElementById("pro").style.color="rgb(75, 250, 75)";
+    document.getElementById("homes").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    
+    }
+    function contact(){
+        document.getElementById("contacts").style.color="rgb(75, 250, 75)";
+        document.getElementById("homes").style.color="white";
+        document.getElementById("pro").style.color="white";
+        document.getElementById("plans").style.color="white";
+        document.getElementById("blogs").style.color="white";
+    
+        }
+        function plan(){
+            document.getElementById("plans").style.color="rgb(75, 250, 75)";
+            document.getElementById("homes").style.color="white";
+            document.getElementById("pro").style.color="white";
+            document.getElementById("contacts").style.color="white";
+            document.getElementById("blogs").style.color="white";
+    
+            }
+            function home(){
+                document.getElementById("homes").style.color="rgb(75, 250, 75)";
+                document.getElementById("pro").style.color="white";
+                document.getElementById("contacts").style.color="white";
+                document.getElementById("plans").style.color="white";
+                document.getElementById("blogs").style.color="white";
+    
+    
+    
+                }
+                function blog(){
+                    document.getElementById("homes").style.color="white";
+                    document.getElementById("pro").style.color="white";
+                    document.getElementById("contacts").style.color="white";
+                    document.getElementById("plans").style.color="white";
+                    document.getElementById("blogs").style.color="rgb(75, 250, 75)";
+    
+        
+        
+                    }
+    
+    
+                function submit(){
+                    let name = document.getElementById("name") ;
+                    let number = document.getElementById("number") ;
+                    let email = document.getElementById("Email") ;
+    
+                    if(name.value == ""){
+                        alert("please Enter Name");
+                    } if(number.value == ""){
+                        alert("please Enter Number");
+                    }
+                     if(email.value =="" ){
+                        alert("please enter e-mail");
+                     }   
+                   
+                    else{
+                        alert("Thanks for Join : "+name.value)
+                    }
+                }
